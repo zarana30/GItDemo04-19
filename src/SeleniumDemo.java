3557e9b6425a50c3feffef19e8f8eccf576ff13a
zarana <zarana30@gmail.com>
2021-04-19 21:56:14 +0530

    Commit from GIT_X

--- src/SeleniumDemo.java
+++ src/SeleniumDemo.java
@@ -4,5 +4,7 @@ public class SeleniumDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Web automation");
+		System.out.println("Web automation1");
+		System.out.println("Web automation2");
 	}
 }
